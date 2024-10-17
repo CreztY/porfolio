55b1d638bc00f1a647a4ac5521f45b30b4f099eb
CreztY <43301930+CreztY@users.noreply.github.com>
2024-10-17 17:13:14 +0200

    1.1
    1.1

--- src/components/contactme.jsx
+++ src/components/contactme.jsx
@@ -5,15 +5,17 @@ function Contactme () {
   return (
     <>
       <div className='contactContainer'>
-        <div className='contactTitle'>
-          <h1><CorreoIcon />Contact Me</h1>
-        </div>
-        <p>Estoy siempre abierto a nuevas oportunidades y colaboraciones. No dudes en contactarme a través del siguiente formulario o mediante mis redes sociales.</p>
-        <button><CorreoIcon />Contactar</button>
-        <div className='socialLinks'>
-          <h3>O sígueme en:</h3>
-          <a href='https://www.linkedin.com/in/ivan-cózar-ramos-a31b47102/' target='_blank' rel='noopener noreferrer'>LinkedIn</a>
-          <a href='https://github.com/CreztY' target='_blank' rel='noopener noreferrer'>GitHub</a>
+        <div className='contactCard'>
+          <div className='contactTitle'>
+            <h1><CorreoIcon />Contact Me</h1>
+          </div>
+          <p>Estoy siempre abierto a nuevas oportunidades y colaboraciones. No dudes en contactarme a través del siguiente formulario o mediante mis redes sociales.</p>
+          <button><CorreoIcon />Contactar</button>
+          <div className='socialLinks'>
+            <h3>O sígueme en:</h3>
+            <a href='https://www.linkedin.com/in/ivan-cózar-ramos-a31b47102/' target='_blank' rel='noopener noreferrer'>LinkedIn</a>
+            <a href='https://github.com/CreztY' target='_blank' rel='noopener noreferrer'>GitHub</a>
+          </div>
         </div>
       </div>
     </>
